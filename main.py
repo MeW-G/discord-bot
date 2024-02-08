c9dce125224d38962814b7333e056f7e096248e0
MeW-G <moritz.gruetzenbach@t-online.de>
2024-02-08 18:03:30 +0100

    schreibt jetzt wenn joins

--- main.py
+++ main.py
@@ -7,24 +7,38 @@ load_dotenv()
 
 token = os.getenv("TOKEN")
 # Erstellen des Bot-Clients
-bot = commands.Bot(command_prefix='-', intents = None)
+bot_intents = discord.Intents.default()
+bot = commands.Bot(command_prefix ='-', intents = bot_intents)
 
+client = bot
+CHANNEL_ID = 1121965331737481258  # Replace with the actual channel ID
 # Event-Handler für das Event 'on_ready' (wird aufgerufen, wenn der Bot erfolgreich gestartet ist)
 @bot.event
 async def on_ready():
     print(f'Eingeloggt als {bot.user.name} ({bot.user.id})')
     print('------')
 
+# Specify the channel ID where you want the bot to send the message
+
+
+# Event to run when the bot is ready
+    # Fetch the channel object
+    channel = client.get_channel(CHANNEL_ID)
+
+    if channel:
+        # Send a message to the specified channel
+        await channel.send("I'm online now!")
+
 # Event-Handler für das Event 'on_message' (wird aufgerufen, wenn eine Nachricht im Server empfangen wird)
 @bot.event
 async def on_message(message):
-    # Verhindern, dass der Bot seine eigenen Nachrichten liest (um Endlosschleifen zu vermeiden)
-    if message.author == bot.user:
+    print("a message has been sent")
+    if message.author == bot.user: #simple guard against bots
         return
-    # Nachrichten im gewünschten Kanal lesen
-
-
     # Wichtig: Die Zeile unten nicht vergessen, um die anderen Event-Handler (wie on_message) weiterhin zu aktivieren
+    if message.content:
+        await message.channel.send(message.content)
+
     await bot.process_commands(message)
 
 # Bot mit dem Discord-Server verbinden (token aus .env)
--- parser/Class_parser.py
+++ parser/Class_parser.py
@@ -1,18 +1,32 @@
-def parsemMessage(string, alphabet):
-    current = ""
-    argumentList = []
-    charList = []
-    for char in string:
-        charList.append(char)
-    for char in charList:
-        if char in alphabet.returners: 
-            return argumentList
-        if char in alphabet.seperators: 
-            argumentList.append(current)
-            current = ""
-        else: 
-            current.append(char)
-    return argumentList
+import Class_alphabet
 
-def parseArguments(arguments, functions):
-    return
+class CustomException(Exception):
+    def __init__(self, message):
+        super().__init__(message)
+
+class Parser:
+    def __init__(self, alphabet) -> None:
+        self.alphabet = alphabet 
+
+    def parsemMessage(string, alphabet):
+        current = ""
+        argumentList = []
+        charList = []
+        for char in string:
+            charList.append(char)
+        for char in charList:
+            if char in alphabet.returners: 
+                return argumentList
+            if char in alphabet.seperators: 
+                argumentList.append(current)
+                current = ""
+            else: 
+                current.append(char)
+        return argumentList
+
+    def parseArguments(arguments, alphabet):
+        for argument in arguments:
+            if argument not in alphabet:
+                raise CustomException("Invalid Argument")
+            
+            
